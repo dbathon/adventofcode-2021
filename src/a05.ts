9c7040f8fe8827263b331bd6671e87edcf3d5fb7
Dominik Bathon <dominik.bathon@gmail.com>
2021-12-05 12:07:44 +0100

    Solve day 5

--- src/a05.ts
+++ src/a05.ts
@@ -1,3 +1,39 @@
+import { Map2D } from "./util/map2D";
 import { p, readLines } from "./util/util";
 
-const lines = readLines("input/a05.txt");
+const lines = readLines("input/a05.txt").map((line) => line.split(/[^\d]+/).map((num) => parseInt(num)));
+
+function drawLine(map: Map2D<number>, line: number[]) {
+  let [x, y, toX, toY] = line;
+  const stepX = Math.sign(toX - x);
+  const stepY = Math.sign(toY - y);
+  map.set(x, y, (map.get(x, y) || 0) + 1);
+  while (x !== toX || y !== toY) {
+    x += stepX;
+    y += stepY;
+    map.set(x, y, (map.get(x, y) || 0) + 1);
+  }
+}
+
+const map = new Map2D<number>();
+const map2 = new Map2D<number>();
+
+for (const line of lines) {
+  if (line[0] == line[2] || line[1] == line[3]) {
+    drawLine(map, line);
+  }
+  drawLine(map2, line);
+}
+
+function countAboveOne(map: Map2D<number>) {
+  let cnt = 0;
+  map.forEach((x, y, value) => {
+    if (value && value > 1) {
+      ++cnt;
+    }
+  });
+  return cnt;
+}
+
+p(countAboveOne(map));
+p(countAboveOne(map2));
