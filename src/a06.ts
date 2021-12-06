8c293660cc0df4b63788b9aa565cf746871a7186
Dominik Bathon <dominik.bathon@gmail.com>
2021-12-06 11:07:30 +0100

    Solve day 6

--- src/a06.ts
+++ src/a06.ts
@@ -1,3 +1,29 @@
-import { p, readLines } from "./util/util";
+import { p, readLines, sum } from "./util/util";
 
 const lines = readLines("input/a06.txt");
+
+const startAges = lines[0].split(",").map((n) => parseInt(n));
+
+function afterGenerations(startAges: number[], generations: number): number {
+  // the index is the age and the value is the count
+  let state: number[] = [];
+  for (const age of startAges) {
+    state[age] = (state[age] || 0) + 1;
+  }
+  for (let i = 0; i < generations; i++) {
+    let newState: number[] = [];
+    state.forEach((count, age) => {
+      let nextAge = age - 1;
+      if (age === 0) {
+        newState[8] = count;
+        nextAge = 6;
+      }
+      newState[nextAge] = (newState[nextAge] || 0) + count;
+    });
+    state = newState;
+  }
+  return sum(state);
+}
+
+p(afterGenerations(startAges, 80));
+p(afterGenerations(startAges, 256));
