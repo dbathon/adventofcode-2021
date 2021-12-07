e473e95f482bab1f3bd1bda1f7498f6ea1c8aea5
Dominik Bathon <dominik.bathon@gmail.com>
2021-12-07 10:08:43 +0100

    Solve day 7

--- src/a07.ts
+++ src/a07.ts
@@ -1,3 +1,30 @@
-import { p, readLines } from "./util/util";
+import { findMax, p, readLines, sum } from "./util/util";
 
 const lines = readLines("input/a07.txt");
+
+const positions = lines[0].split(",").map((n) => parseInt(n));
+
+const min = findMax(positions, (pos) => -pos).maxElement!;
+const max = findMax(positions, (pos) => pos).maxElement!;
+
+function requiredFuel(positions: number[], target: number) {
+  return sum(positions.map((pos) => Math.abs(pos - target)));
+}
+
+const targets: number[] = [];
+for (let i = min; i <= max; i++) {
+  targets.push(i);
+}
+
+p(-findMax(targets, (target) => -requiredFuel(positions, target)).max!);
+
+function requiredFuel2(positions: number[], target: number) {
+  return sum(
+    positions.map((pos) => {
+      const dist = Math.abs(pos - target);
+      return (dist * (dist + 1)) / 2;
+    })
+  );
+}
+
+p(-findMax(targets, (target) => -requiredFuel2(positions, target)).max!);
