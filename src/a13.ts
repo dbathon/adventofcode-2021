28feb36161b0369d675147ed3c6178ff57c0f420
Dominik Bathon <dominik.bathon@gmail.com>
2021-12-13 21:11:44 +0100

    Solve day 13

--- src/a13.ts
+++ src/a13.ts
@@ -1,3 +1,39 @@
+import { Map2D } from "./util/map2D";
 import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a13.txt");
+
+class Point {
+  constructor(readonly x: number, readonly y: number) {}
+}
+
+const inputPoints: Point[] = [];
+
+const foldFunctions: ((p: Point) => Point)[] = [];
+
+for (const line of lines) {
+  if (line.startsWith("fold along x=")) {
+    const x = parseInt(line.split("=")[1]);
+    foldFunctions.push((p) => (p.x < x ? p : new Point(x - (p.x - x), p.y)));
+  } else if (line.startsWith("fold along y=")) {
+    const y = parseInt(line.split("=")[1]);
+    foldFunctions.push((p) => (p.y < y ? p : new Point(p.x, y - (p.y - y))));
+  } else {
+    const [x, y] = line.split(",");
+    inputPoints.push(new Point(parseInt(x), parseInt(y)));
+  }
+}
+
+p(new Set(inputPoints.map(foldFunctions[0]).map((p) => p.x + ", " + p.y)).size);
+
+const map: Map2D<string> = new Map2D();
+
+for (const point of inputPoints) {
+  let p = point;
+  for (const foldFunction of foldFunctions) {
+    p = foldFunction(p);
+  }
+  map.set(p.x, p.y, "#");
+}
+
+p(map.draw());
