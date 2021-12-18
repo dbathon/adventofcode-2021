8da5266eafe1efb4b8e2df8e2514aedec2444c22
Dominik Bathon <dominik.bathon@gmail.com>
2021-12-18 10:22:27 +0100

    Solve day 18

--- src/a18.ts
+++ src/a18.ts
@@ -1,3 +1,139 @@
 import { p, readLines } from "./util/util";
 
 const lines = readLines("input/a18.txt");
+
+type SNumber = [SNumber | number, SNumber | number];
+
+function explodeOne(input: SNumber, depth = 0): { leftAdd?: number; replace?: number; rightAdd?: number } | undefined {
+  const [a, b] = input;
+
+  if (depth >= 4 && typeof a === "number" && typeof b === "number") {
+    return { leftAdd: a, replace: 0, rightAdd: b };
+  }
+
+  function recAdd(sNumber: SNumber, pos: 0 | 1, toAdd: number) {
+    const part = sNumber[pos];
+    if (typeof part === "number") {
+      sNumber[pos] = part + toAdd;
+    } else {
+      recAdd(part, pos, toAdd);
+    }
+  }
+
+  if (typeof a !== "number") {
+    const result = explodeOne(a, depth + 1);
+    if (result) {
+      if (result.replace !== undefined) {
+        input[0] = result.replace;
+        result.replace = undefined;
+      }
+      if (result.rightAdd !== undefined) {
+        if (typeof b == "number") {
+          input[1] = b + result.rightAdd;
+        } else {
+          recAdd(b, 0, result.rightAdd);
+        }
+        result.rightAdd = undefined;
+      }
+      return result;
+    }
+  }
+
+  if (typeof b !== "number") {
+    const result = explodeOne(b, depth + 1);
+    if (result) {
+      if (result.replace !== undefined) {
+        input[1] = result.replace;
+        result.replace = undefined;
+      }
+      if (result.leftAdd !== undefined) {
+        if (typeof a == "number") {
+          input[0] = a + result.leftAdd;
+        } else {
+          recAdd(a, 1, result.leftAdd);
+        }
+        result.leftAdd = undefined;
+      }
+      return result;
+    }
+  }
+
+  return undefined;
+}
+
+function splitOne(input: SNumber): boolean {
+  const [a, b] = input;
+
+  function split(num: number): SNumber {
+    const a = Math.floor(num / 2);
+    return [a, num - a];
+  }
+
+  if (typeof a === "number") {
+    if (a > 9) {
+      input[0] = split(a);
+      return true;
+    }
+  } else if (splitOne(a)) {
+    return true;
+  }
+
+  if (typeof b === "number") {
+    if (b > 9) {
+      input[1] = split(b);
+      return true;
+    }
+  } else if (splitOne(b)) {
+    return true;
+  }
+
+  return false;
+}
+
+function reduce(input: SNumber) {
+  while (true) {
+    if (explodeOne(input)) {
+      continue;
+    }
+    if (splitOne(input)) {
+      continue;
+    }
+    break;
+  }
+}
+
+function add(a: SNumber, b: SNumber): SNumber {
+  const result: SNumber = [a, b];
+  reduce(result);
+  return result;
+}
+
+function magnitude(input: SNumber | number): number {
+  if (typeof input === "number") {
+    return input;
+  } else {
+    const [a, b] = input;
+    return 3 * magnitude(a) + 2 * magnitude(b);
+  }
+}
+
+function clone<T>(input: T): T {
+  return JSON.parse(JSON.stringify(input));
+}
+
+const sNumbers: SNumber[] = lines.map((line) => JSON.parse(line));
+
+p(magnitude(clone(sNumbers).reduce(add)));
+
+let max = 0;
+
+for (let i = 0; i < sNumbers.length; i++) {
+  const a = sNumbers[i];
+  for (let j = i + 1; j < sNumbers.length; j++) {
+    const b = sNumbers[j];
+    max = Math.max(max, magnitude(add(clone(a), clone(b))));
+    max = Math.max(max, magnitude(add(clone(b), clone(a))));
+  }
+}
+
+p(max);
